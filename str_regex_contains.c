1a37a7134f48fc6e868e472889ffb424e8ac706e
ammicco1 <84914236+ammicco@users.noreply.github.com>
2022-05-02 14:18:50 +0200

    ok

--- str_regex_contains.c
+++ str_regex_contains.c
@@ -3,23 +3,23 @@
 
 #include "./str_regex_contains.h"
 
-#define REG_MATCH_SIZE 10
+#define _MATCH_SIZE_ 10
 
 int str_regex_contains(char *str, char *pattern){
-	regmatch_t match[REG_MATCH_SIZE];
-    regex_t regex;
+	regmatch_t match[_MATCH_SIZE_];
+  regex_t regex;
 
-    if(str == NULL){
+  if(str == NULL){
 		return -1;
 	}
 
   if(regcomp(&regex, pattern, REG_ICASE | REG_EXTENDED) != 0){
-      regfree(&regex);         
+    regfree(&regex);         
   
-      return -1;
+    return -1;
   }
 
-  if(!regexec(&regex, str, (size_t) REG_MATCH_SIZE, match, 0)){
+  if(!regexec(&regex, str, (size_t) _MATCH_SIZE_, match, 0)){
 	  regfree(&regex);
 	
 	  return 1;
