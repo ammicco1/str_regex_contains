f2a9045d5e887d1dd536faed774fef2f960c9125
ammicco1 <84914236+ammicco@users.noreply.github.com>
2022-05-02 13:48:04 +0200

    :no:

--- test.c
+++ test.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char **argv){
     if(argc < 3){
-        fprintf(stderr, "no str or regex! use main str regex");
+        fprintf(stderr, "no str or regex! use main str regex\n");
         
         return -1;
     }
